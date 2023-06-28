1bd3e8a1a6faa8d2e2f105840a94cf678ea59f5a
MarxGithub225 <97442575+mnmyproj@users.noreply.github.com>
2023-06-28 20:20:55 +0000

    Provider core components

---
+++ tims-provider-app/src/@core/components/noDataComponent/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { FormattedMessage } from "react-intl"
+
+function NoData() {
+  return <div style={{padding: "15px 0px", display: 'flex', alignItems: "center", color: "#263238"}}>Il n'y a aucune donnée</div>
+}
+
+export default NoData;
